b6169285fa7f1ae9afa5efb2b6d79cebdc939137
OumaArera <johnouma999@gmail.com>
2025-02-04 11:41:07 +0300

    feat: complete

--- patient_project/settings.py
+++ patient_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-DOMAIN = os.getenv("DOMAIN", "http://127.0.0.1:8000")
+DOMAIN = os.getenv("DOMAIN")
 MEDIA_FULL_URL = f"{DOMAIN}"
 
 DEFAULT_PAGE_SIZE = 5
