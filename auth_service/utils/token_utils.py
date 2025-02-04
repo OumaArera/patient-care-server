c401fec70f59a4e3cf94bf1f2b5600b92fc0cbb8
OumaArera <johnouma999@gmail.com>
2025-02-04 21:23:36 +0300

    feat: complete

--- auth_service/utils/token_utils.py
+++ auth_service/utils/token_utils.py
@@ -11,6 +11,7 @@ def generate_jwt_token(user):
 	payload = {
 		"user_id": user.id,
 		"username": user.username,
+		"patients": user.patients,
 		"fullName": f"{user.firstName} {user.lastName}",
 		"role": user.role,
 		"iss": JWT_ISSUER,
