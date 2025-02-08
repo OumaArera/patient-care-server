1b4016f242231ea0afb9d266081e842f3f896b4d
OumaArera <johnouma999@gmail.com>
2025-02-08 13:10:46 +0300

    feat: complete

--- custom_admin/models/patient_manager.py
+++ custom_admin/models/patient_manager.py
@@ -6,19 +6,22 @@ class PatientManager(models.Model):
     """Create PatientManager"""
     patientManagerId = models.AutoField(primary_key=True)
     patient =  models.ForeignKey(
-		Patient,
-		on_delete=models.CASCADE,
-		related_name='patient_managers'
-	)
+      Patient,
+      on_delete=models.CASCADE,
+      related_name='patient_managers'
+    )
     careGiver =  models.ForeignKey(
-		User,
-		null=True,
-		blank=True,
-		on_delete=models.SET_NULL,
-		related_name='patient_managers'
-	)
+      User,
+      null=True,
+      blank=True,
+      on_delete=models.SET_NULL,
+      related_name='patient_managers'
+    )
     createdAt = models.DateTimeField(auto_now_add=True)
     modifiedAt = models.DateTimeField(auto_now=True)
+
+    class Meta:
+      unique_together = ('patient', 'careGiver') 
 
     @classmethod
     def create_patient_manager(cls, validated_data):
