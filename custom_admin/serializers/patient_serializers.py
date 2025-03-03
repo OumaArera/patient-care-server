9912fe5f3248ea47efb68437cc8daae090f7b735
OumaArera <johnouma999@gmail.com>
2025-03-03 10:56:34 +0300

    feat: complete

--- custom_admin/serializers/patient_serializers.py
+++ custom_admin/serializers/patient_serializers.py
@@ -11,9 +11,9 @@ class PatientSerializer(serializers.ModelSerializer):
     branch = serializers.PrimaryKeyRelatedField(queryset=Branch.objects.all(), required=True)
     diagnosis = serializers.CharField(required=False, allow_null=True, allow_blank=True)
     allergies = serializers.CharField(required=False, allow_null=True, allow_blank=True)
-    clinician = serializers.CharField(required=False)
-    room = serializers.CharField(required=False)
-    cart = serializers.CharField(required=False)
+    clinician = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    room = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    cart = serializers.CharField(required=False, allow_null=True, allow_blank=True)
 
     def validate_dateOfBirth(self, value):
         """Ensure the patient is not older than 120 years."""
@@ -21,15 +21,17 @@ class PatientSerializer(serializers.ModelSerializer):
         age = today.year - value.year - ((today.month, today.day) < (value.month, value.day))
         if age > 120:
             raise serializers.ValidationError("Age cannot be greater than 120 years.")
+        if age < 5:
+            raise serializers.ValidationError("Age cannot be less than 5 years.")
         return value
 
     class Meta:
         model = Patient
         fields = [
-            "patientId", "firstName", "middleNames", "lastName", "dateOfBirth",
+            "patientId", "firstName", "middleNames", "lastName", "dateOfBirth", "active",
             "diagnosis", "allergies", "physicianName", "pcpOrDoctor", "branch", "room", "cart", "clinician"
         ]
-        read_only_fields = ["patientId"]
+        read_only_fields = ["patientId", "active"]
 
 
 
@@ -38,13 +40,13 @@ class PatientUpdateSerializer(serializers.ModelSerializer):
     middleNames = serializers.CharField(required=False, allow_null=True, allow_blank=True)
     firstName = serializers.CharField(required=False)
     lastName = serializers.CharField(required=False)
-    diagnosis = serializers.CharField(required=False)
-    allergies = serializers.CharField(required=False)
+    diagnosis = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    allergies = serializers.CharField(required=False, allow_null=True, allow_blank=True)
     physicianName = serializers.CharField(required=False)
     pcpOrDoctor = serializers.CharField(required=False)
     clinician = serializers.CharField(required=False)
-    room = serializers.CharField(required=False)
-    cart = serializers.CharField(required=False)
+    room = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    cart = serializers.CharField(required=False, allow_null=True, allow_blank=True)
     dateOfBirth = serializers.DateField(required=False)
     branch = serializers.PrimaryKeyRelatedField(queryset=Branch.objects.all(), required=False)
     active = serializers.BooleanField(required=False)
